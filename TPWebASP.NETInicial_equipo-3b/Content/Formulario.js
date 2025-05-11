3c2fc6f995757776c4ebd03ed8d45206ac64a51f
Lucia <schirolucia05@gmail.com>
2025-05-11 14:24:33 -0300

    validacion de campos form completos

--- TPWebASP.NETInicial_equipo-3b/Content/Formulario.js
+++ TPWebASP.NETInicial_equipo-3b/Content/Formulario.js
@@ -6,70 +6,6 @@ window.onload = function () {
     }
 }
 
-//function validarNumeros() {
-//    const dni = document.getElementById("dniText").value;
-//    const cp = document.getElementById("codigoPostal").value;
-//    const mensajeDNI = document.getElementById("lbldni");
-//    const mensajeCP = document.getElementById("lblCp");
-
-
-//    if (dni.trim() === "" || !/^\d+$/.test(dni)) {
-//        mensajeDNI.textContent = "Este campo debe ser numérico y no estar vacío.";
-//        return false;
-//    }
-
-//    if (dni.length !== 8) {
-//        mensajeDNI.textContent = "El campo debe contener 8 dígitos.";
-//        return false;
-//    }
-
-//    if (cp.trim() === "" || !/^\d+$/.test(cp)) {
-//        mensajeCP.textContent = "Este campo debe ser numérico y no estar vacío.";
-//        return false;
-//    }
-
-//    return true;
-//}
-
-//function validarLetras() {
-//    const nombre = document.getElementById("nombre").value;
-//    const apellido = document.getElementById("apellido").value;
-//    const ciudad = document.getElementById("ciudad").value;
-//    const mensajeNombre = document.getElementById("lblNombre");
-//    const mensajeApellido = document.getElementById("lblApellido");
-//    const mensajeCiudad = document.getElementById("lblCiudad")
-
-//    let esValido = true;
-
-//        if (nombre === "") {
-//            esValido = false;
-//            mensajeNombre.textContent = "Este campo no puede estar vacío";
-//        } else if (!/^[A-Za-z]+$/.test(nombre)) {
-//            esValido = false;
-//            mensajeNombre.textContent = "El nombre solo puede contener letras";
-//        }
-
-//        if (apellido === "") {
-//            esValido = false;
-//            mensajeApellido.textContent = "Este campo no puede quedar vacío";
-//        } else if (!/^[A-Za-z]+$/.test(apellido)) {
-//            esValido = false;
-//            mensajeApellido.textContent = "El apellido solo puede contener letras";
-//        }
-
-//        if (ciudad === "") {
-//            esValido = false;
-//            mensajeCiudad.textContent = "Este campo no puede quedar vacío";
-//        } else if (!/^[A-Za-z]+$/.test(ciudad)) {
-//            esValido = false;
-//            mensajeCiudad.textContent = "La ciudad solo puede contener letras";
-//        }
-  
-//    return esValido;
-
-//}
-
-
 function validarNumeros(idCampo, idMensaje, longitud = 0) {
     const valor = document.getElementById(idCampo).value;
     const mensaje = document.getElementById(idMensaje);
@@ -149,12 +85,21 @@ function validarCamposVacios(idCampo, idMensaje) {
     return true;
 }
 
-function validarFormulario() {
-    const numerosValidos = validarNumeros();
-    const letrasValidas = validarLetras();
-    validarCamposVacios();
 
-    return numerosValidos && letrasValidas;
+function validarFormulario() {
+    const dniValido = validarNumeros('dniText', 'lbldni', 8);
+    const nombreValido = validarLetras('nombre', 'lblNombre');
+    const apellidoValido = validarLetras('apellido', 'lblApellido');
+    const emailValido = validarEmail('email', 'lblEmail');
+    const direccionValida = validarCamposVacios('direccion', 'lblDireccion');
+    const ciudadValida = validarLetras('ciudad', 'lblCiudad');
+    const cpValido = validarNumeros('codigoPostal', 'lblCp');
+
+    if (!dniValido || !nombreValido || !apellidoValido || !emailValido || !direccionValida || !ciudadValida || !cpValido ) {
+        return false;
+    }
+
+    return true;
 }
 
 
