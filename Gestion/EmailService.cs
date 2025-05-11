df728c49584e0d8d3e0c0c7ee311af08ff7871ee
tu_nombre <luciapazmanagement@gmail.com>
2025-05-11 18:16:29 -0300

    Hago cambios en formulario para enviar correctamente el formulario

--- Gestion/EmailService.cs
+++ Gestion/EmailService.cs
@@ -15,20 +15,20 @@ namespace Gestion
         public EmailService() 
         {
             server = new SmtpClient();
-            server.Credentials = new NetworkCredential("programacioniiiWEB@gmail.com", "programacion3");
+            server.Credentials = new NetworkCredential("programacionwebiii@yahoo.com", "actividad4_");
             server.EnableSsl = true;
-            server.Port = 587;
-            server.Host = "smtp-mail.outlook.com";
+            server.Port = 465;
+            server.Host = "smtp.mail.yahoo.com";
         
         }
 
         public void armarCorreo(string emailDestino)
         {
             email = new MailMessage();
-            email.From = new MailAddress("noresponder@programacioniii.com");
+            email.From = new MailAddress("noresponder@programacionwebiii.com");
             email.To.Add(emailDestino);
             email.IsBodyHtml = true;
-            email.Body = "";
+            email.Body = "<h1>Registro exitoso</h1> <p> ¡Ya estas participando! - Enterate de mas novedades en nuestra web </p>";
         }
 
         public void enviarEmail() 
